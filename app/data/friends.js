579ce362798ddfc3965f23b91a4d246c521f28f8
Christian Saxton <CRS-MACBOOK@CRS-MBP.local>
2018-06-14 18:04:06 -0400

    Match modal now provides a link to the match's github. Added some dummy data to friends.js.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,55 @@
-let friendArray = [];
+let friendArray = [
+    {
+        "name":"Christian Saxton",
+        "location":"Charlotte, NC",
+        "github":"crsaxton87",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name":"Jerry Lewis",
+        "location":"Green Bay, WI",
+        "github":"jerrylewisdummy",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        "name":"Sleepy Gary",
+        "location":"Redmond, WA",
+        "github":"sleepygarydummy",
+        "scores":[
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    }
+];
 
 module.exports = friendArray;
